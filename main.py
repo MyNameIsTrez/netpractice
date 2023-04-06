75aa4188c622e2a87b4280d0237698e51437c4d9
MyNameIsTrez <sbos@student.codam.nl>
2023-04-06 22:53:57 +0200

    Remove unused function

--- main.py
+++ main.py
@@ -100,23 +100,5 @@ def print_interfaces(interfaces):
 			print(f"{interface_name}" + interface_string)
 
 
-def print_routing_tables(routing_tables):
-	print("routing tables:")
-	for routing_table_name, routing_table in routing_tables.items():
-		for route in routing_table:
-			destination_known = route["destination_known"]
-			cidr_known = route["cidr_known"]
-			next_hop_known = route["next_hop_known"]
-			if not destination_known or not cidr_known or not next_hop_known:
-				string = f"{routing_table_name}:"
-				if not destination_known:
-					string += f" destination: {route['destination']}"
-				if not cidr_known:
-					string += f" cidr: {route['cidr']}"
-				if not next_hop_known:
-					string += f" next_hop: {route['next_hop']}"
-				print(string)
-
-
 if __name__ == "__main__":
 	main()
